32ecf0e81198861babe83aeccca4c9f5d5f19022
Paul De Ulloa <pauld143@gmail.com>
2024-11-29 23:54:12 -0800

    notes added to Curry file.

--- JavaScriptCurry/mediumProblem.js
+++ JavaScriptCurry/mediumProblem.js
@@ -7,3 +7,5 @@ var curry = function (fn) {
     return (...nextArgs) => curried(...args, ...nextArgs);
   };
 };
+
+// Currying is a powerful technique in functional programming that transforms a function with multiple arguments into a sequence of functions. It allows you to create flexible and reusable code by enabling partial application of function arguments.
