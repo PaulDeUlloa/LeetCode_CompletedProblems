6325594e940b5fbe6adca2572c88eb1af9e1702d
Paul De Ulloa <pauld143@gmail.com>
2024-12-01 23:44:48 -0800

    promise pool notes added

--- PromisePool/mediumProblem.js
+++ PromisePool/mediumProblem.js
@@ -32,3 +32,9 @@ var promisePool = async function (functions, n) {
  * promisePool([() => sleep(500), () => sleep(400)], 1)
  *   .then(console.log) // After 900ms
  */
+
+//! We can keep track of current index in the functions array (functionIndex) and the current number of promises being executed (inProgressCount). We define a recursive function helper which will allow us to execute code asynchronously. All this code is wrapped in the returned promise's callback.
+
+// 1. Every time we execute a new function, we increment functionIndex and we increment inProgressCount.
+// 2. Every time a promise resolves, we decrement inProgressCount, and repeat step 1 while inProgressCount < n and there are still functions left to execute
+// 3. If at any point, functionIndex == functions.length and inProgressCount == 0, we are done and should resolve the returned promise.
