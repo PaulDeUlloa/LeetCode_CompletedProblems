5deb52c0c04e4f0f34b555367a1834bc457aa87b
Paul De Ulloa <pauld143@gmail.com>
2024-12-09 23:58:58 -0800

    javascript curry notes added

--- JavaScriptCurry/mediumProblem.js
+++ JavaScriptCurry/mediumProblem.js
@@ -9,3 +9,5 @@ var curry = function (fn) {
 };
 
 // Currying is a powerful technique in functional programming that transforms a function with multiple arguments into a sequence of functions. It allows you to create flexible and reusable code by enabling partial application of function arguments.
+
+//These three dots are called the spread syntax or spread operator. The spread syntax is a feature of ES6, and it's also used in React. Spread syntax allows you to deconstruct an array or object into separate variables
