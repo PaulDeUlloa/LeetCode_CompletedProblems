44c53cc245ca229ad90c2c28fd078dc8395c4d85
Paul De Ulloa <pauld143@gmail.com>
2024-12-27 16:06:38 -0800

    solution added

--- MiddleOfTheLinkedList/easyProblem.js
+++ MiddleOfTheLinkedList/easyProblem.js
@@ -9,4 +9,10 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var middleNode = function (head) {};
+var middleNode = function (head) {
+  let A = [head];
+
+  while (A[A.length - 1].next != null) A.push(A[A.length - 1].next);
+
+  return A[Math.trunc(A.length / 2)];
+};
