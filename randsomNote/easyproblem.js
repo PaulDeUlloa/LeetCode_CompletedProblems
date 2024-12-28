8e86118ed7271b686f01e4c23249467104bae401
Paul De Ulloa <pauld143@gmail.com>
2024-12-28 14:28:50 -0800

    solution added

--- randsomNote/easyproblem.js
+++ randsomNote/easyproblem.js
@@ -1,3 +1,36 @@
 // Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
 
 // Each letter in magazine can only be used once in ransomNote.
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  const makeCountsMap = (s) => {
+    const counts = {};
+    for (const c of s) {
+      counts[c] = (counts[c] || 0) + 1;
+    }
+    return counts;
+  };
+
+  if (ransomNote.length > magazine.length) {
+    return false;
+  }
+
+  const ransomNoteCounts = makeCountsMap(ransomNote);
+  const magazineCounts = makeCountsMap(magazine);
+
+  for (const c in ransomNoteCounts) {
+    const countInMagazine = magazineCounts[c] || 0;
+    const countInRansomNote = ransomNoteCounts[c];
+
+    if (countInMagazine < countInRansomNote) {
+      return false;
+    }
+  }
+
+  return true;
+};
