fc64972827b6a2f2953b35e0fc0705819e088891
Paul De Ulloa <pauld143@gmail.com>
2025-01-12 17:17:14 -0800

    Approach 3 added.

--- romanToIntegar/easyProblem.js
+++ romanToIntegar/easyProblem.js
@@ -72,3 +72,37 @@ var romanToInt = function (s) {
 
   return total;
 };
+
+//! Approach 3: Right-to-Left Pass
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var romanToInt = function (s) {
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let lastSymbol = s[s.length - 1];
+  let lastValue = values[lastSymbol];
+  let total = lastValue;
+
+  for (let i = s.length - 2; i >= 0; i--) {
+    let currentSymbol = s[i];
+    let currentValue = values[currentSymbol];
+    if (currentValue < lastValue) {
+      total -= currentValue;
+    } else {
+      total += currentValue;
+    }
+    lastValue = currentValue;
+  }
+  return total;
+};
