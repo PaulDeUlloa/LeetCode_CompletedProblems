be38282521f94d53b8a39610d410bfaa4b4ee7a1
Paul De Ulloa <pauld143@gmail.com>
2025-01-12 17:04:11 -0800

    approach 2 added

--- romanToIntegar/easyProblem.js
+++ romanToIntegar/easyProblem.js
@@ -34,3 +34,41 @@ var romanToInt = function (s) {
 };
 
 //! Approach 2: Left-to-Right Pass Improved
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+let values = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+  IV: 4,
+  IX: 9,
+  XL: 40,
+  XC: 90,
+  CD: 400,
+  CM: 900,
+};
+
+var romanToInt = function (s) {
+  let total = 0;
+  let i = 0;
+
+  while (i < s.length) {
+    if (i < s.length - 1 && s.substring(i, i + 2) in values) {
+      total += values[s.substring(i, i + 2)];
+      i += 2;
+    } else {
+      total += values[s.charAt(i)];
+      i++;
+    }
+  }
+
+  return total;
+};
