f7b8197ae7d235bb9e6648a729d75d2227b935d8
Paul De Ulloa <pauld143@gmail.com>
2025-01-13 21:14:25 -0800

    notes added

--- mergeTwoSortedLists/easyProblem.js
+++ mergeTwoSortedLists/easyProblem.js
@@ -64,3 +64,19 @@ var mergeTwoLists = function (list1, list2) {
   }
   return prehead.next;
 };
+
+//! Algorithm:
+// First, we set up a false "prehead" node that allows us to easily return the
+// head of the merged list later. We also maintain a prev pointer, which
+// points to the current node for which we are considering adjusting its next
+// pointer. Then, we do the following until at least one of l1 and l2 points
+// to null: if the value at l1 is less than or equal to the value at l2,
+// then we connect l1 to the previous node and increment l1. Otherwise, we
+// do the same, but for l2. Then, regardless of which list we connected, we
+// increment prev to keep it one step behind one of our list heads.
+
+// After the loop terminates, at most one of l1 and l2 is non-null.
+// Therefore (because the input lists were in sorted order), if either list is
+// non-null, it contains only elements greater than all of the
+// previously-merged elements. This means that we can simply connect the
+// non-null list to the merged list and return it.
