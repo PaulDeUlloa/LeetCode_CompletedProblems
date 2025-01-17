bb1e37ce62f4b2f71b6cee613809e4b8d22124d3
Paul De Ulloa <pauld143@gmail.com>
2025-01-16 19:07:10 -0800

    initial commit

---
+++ longestCommonPrefix/easyProblem.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  //we need to iterate through the string length
+  //if there is no common prefix, return an empty string ""
+  // we need to return
+};
