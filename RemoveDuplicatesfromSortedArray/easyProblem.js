221bc6348c1123124cc53dd9c8c02c028d264393
Paul De Ulloa <pauld143@gmail.com>
2025-02-03 21:38:22 -0800

    two indexes approach solution added

--- RemoveDuplicatesfromSortedArray/easyProblem.js
+++ RemoveDuplicatesfromSortedArray/easyProblem.js
@@ -29,4 +29,17 @@ If all assertions pass, then your solution will be accepted.
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function (nums) {};
+var removeDuplicates = function (nums) {
+  let insertIndex = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    //here is where we skip an index if we see a duplicate element.
+    if (nums[i - 1] != nums[i]) {
+      //here we store the unique element at insertIndex index and incrementing the insertIndex by 1
+      nums[insertIndex] = nums[i];
+      insertIndex++;
+    }
+  }
+
+  return insertIndex;
+};
