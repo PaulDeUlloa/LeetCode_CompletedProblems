74c6a77e51f3af9bc7df837933304c838f60591c
Paul De Ulloa <pauld143@gmail.com>
2025-03-02 20:13:29 -0800

    solution 1 - two pointers approach

--- RemoveElement/easyProblem.js
+++ RemoveElement/easyProblem.js
@@ -16,4 +16,13 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function (nums, val) {};
+var removeElement = function (nums, val) {
+  var i = 0;
+  for (var j = 0; j < nums.length; j++) {
+    if (nums[j] != val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  return i;
+};
